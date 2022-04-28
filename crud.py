0eaa443e71e9f8612d55068ed5dcc9d167c33ef5
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-04-28 11:56:43 -0700

    Completed Pt 4, up to Further Study

--- crud.py
+++ crud.py
@@ -29,6 +29,11 @@ def get_user_by_id(user_id):
     return User.query.get(user_id)
 
 
+def get_user_by_email(email):
+    """Returns a user by email"""
+
+    return User.query.filter(User.email == email).first()
+
 
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
--- server.py
+++ server.py
@@ -45,6 +45,32 @@ def all_users():
 
     return render_template('all_users.html', users=users)
 
+@app.route('/users', methods=["POST"])
+def register_user():
+    """Register a new user."""
+
+    #retrieve data from request.form
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
+    #if user with this email exists:
+    if (crud.get_user_by_email(email)):
+        #flash a message to tell user they can't create accoun
+        flash('Your account already exists!')
+    #otherwise
+    else:
+        #create a new user
+        new_user = crud.create_user(email, password)
+        #add to database.
+        db.session.add(new_user)
+        #commit to database
+        db.session.commit()
+        #flash message telling them success
+        flash('Success! Account created.')
+
+    #redirect back to homepage
+    return redirect('/')
+
 @app.route('/users/<user_id>')
 def show_user(user_id):
     """Show details on a particular user."""
@@ -52,7 +78,28 @@ def show_user(user_id):
     user = crud.get_user_by_id(user_id)
     
     return render_template('user_details.html', user=user)
+    
+@app.route('/login', methods=['POST'])
+def login():
+    """Log in a particular user."""
 
+    #pull info from form submission
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    #query for user with given email
+    user = crud.get_user_by_email(email)
+    #make sure password matches
+    if password == user.password:
+        #add primary key to flask session
+        session['user'] = user.user_id
+        #flash success message
+        flash('Successfully logged in!')
+    else:
+        flash('Wrong password :(')
+        return redirect('/')
+
+    return redirect('/movies')
 
 
 if __name__ == "__main__":
